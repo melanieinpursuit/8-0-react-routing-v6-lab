58fab83af711fa6ee13af69fbcdb0e0598228ca4
melanieinpursuit <melaniegonzalez@pursuit.org>
2022-11-01 15:24:11 -0400

    Almost all tests passing, not sure why first cat link test is not passing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from "react";
 
 /*
@@ -25,11 +26,24 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+      <Router>
+        <Nav />
+          <Routes>
+            <Route
+              path="/"
+              element={<Home employees={employees} owners={owners} pets={pets} />} />
+            <Route
+              path="/staff"
+              element={<StaffList employees={employees} />} />
+            <Route 
+              path="/pets"
+              element={<PetsList pets={pets} />} />
+            <Route
+              path="/pets/:kind"
+              element={<PetsList pets={pets} />} />
+          </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,8 +1,11 @@
+import { useParams, Link } from "react-router-dom";
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { petData } from "../../data/pets";
 
 export const PetsList = ({ pets }) => {
+  let { kind } = useParams()
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -10,21 +13,22 @@ export const PetsList = ({ pets }) => {
       return acc;
     },
     [[], []]
-  );
-
+    );
+    
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
+
         {/* All cats section */}
-        {cats.map((cat) => (
+        {kind === 'cats' || kind === undefined ? cats.map((cat) => (
           <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+          )) : null}
 
         {/* All dogs section */}
-        {dogs.map((dog) => (
+        {kind === 'dogs' || kind === undefined ? dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        )) : null}
       </section>
     </section>
   );
